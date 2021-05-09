640dd94f4ac9ce7f4e29af6229a59aa2b1270602
dimash12 <48655470+dimash12@users.noreply.github.com>
2021-05-09 15:19:50 -0500

    Update ec2.bat

--- ec2.bat
+++ ec2.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-0b697c4ae566cad55 --count 1 --instance-type t2.micro --key-name ade --security-groups adesc
+aws ec2 run-instances --image-id ami-0b697c4ae566cad55 --count 1 --instance-type t2.micro --key-name ade --security-groups adesc --region us-east-2
